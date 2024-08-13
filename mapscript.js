d1a8a3576fe8184eaa83ee9b903fc3254af95bc0
Adam Brailove <adam.brailove@gmail.com>
2024-08-13 09:37:56 -0700

    cleanup

--- mapscript.js
+++ mapscript.js
@@ -28,10 +28,15 @@ var map = new maplibregl.Map({
     zoom: 8 // starting zoom
 });
 
-
+// Create a popup, but don't add it to the map yet.
+const popup = new maplibregl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
 
 map.on('load', () => {
-    fetch('./stations.json')        // CORS PROBLEM with this: fetch('http://127.0.0.1:3000/api/stations')
+    fetch('./stations.json')
+        //fetch('http://127.0.0.1:3000/api/stations') // CORS PROBLEM with this
         .then((response) => response.json())
         .then((stations) => {
             const geo = build_geojson_stations(stations)
@@ -63,55 +68,61 @@ map.on('load', () => {
         });
 });
 
-// When a click event occurs on a feature in the layer, open a popup at the
-// location of the feature, with description HTML from its properties.
+// When a click event occurs on a feature in the layer toggle it's is_selected property
 map.on('click', 'stations_layer', (e) => {
-    let feature = e.features[0];
-    const coordinates = feature.geometry.coordinates.slice();
-    const station_name = feature.properties.station_name;
-    const id = feature.id;
-    const station_id = feature.properties.station_id;
-    console.log("Found ID: ", id);
+    // Get the id (index) of the clicked feature from the event
+    const id = e.features[0].id;
+
+    // Get the geojson data from the map source
     map.getSource('stations_data').getData()
         .then((geojson_data) => {
+            // Find the clicked feature by id
             let feature = geojson_data.features[id];
+
+            // Toggle it's is_selected state
             const new_is_selected = !feature.properties.is_selected;
-            feature.properties.is_selected = new_is_selected; // Toggle selected state
-            console.log("New is_selected state: ", new_is_selected);
+            feature.properties.is_selected = new_is_selected;
+            console.log(`User clicked feature ID: ${id}.  Selected = ${new_is_selected}`);
+
+            // Write updated data to map source
             map.getSource('stations_data').setData(geojson_data);   // Write the updated data
-
-            // --- SHOW POPUP ---
-            // Ensure that if the map is zoomed out such that multiple
-            // copies of the feature are visible, the popup appears
-            // over the copy being pointed to.
-            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-            }
-            new maplibregl.Popup()
-                .setLngLat(coordinates)
-                .setHTML(
-                    `<pre><strong>Station:</strong> ${station_name}\n<strong>ID:</strong> ${station_id}\n<strong>Selected:</strong> ${new_is_selected}</pre>`)
-                .addTo(map);
         });
-
-
 });
 
 // Change the cursor to a pointer when the mouse is over the places layer.
-map.on('mouseenter', 'stations_layer', () => {
+map.on('mouseenter', 'stations_layer', (e) => {
+    // Change the cursor style as a UI indicator.
     map.getCanvas().style.cursor = 'pointer';
+
+    let feature = e.features[0];
+    const coordinates = feature.geometry.coordinates.slice();
+    const station_name = feature.properties.station_name;
+    //const id = feature.id;
+    const station_id = feature.properties.station_id;
+    //const selected = feature.properties.is_selected;
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    popup.setLngLat(coordinates).setHTML(`<pre>${station_name}\n${station_id}</pre>`).addTo(map);
 });
 
 // Change it back to a pointer when it leaves.
 map.on('mouseleave', 'stations_layer', () => {
     map.getCanvas().style.cursor = '';
+    popup.remove();
 });
 
 function build_geojson_stations(stations) {
     geojson_array = [];
     var index = 0;
     for (const station of stations) {
-        //console.log(">>>", index, station.station_name, station.longitude, station.latitude, station.station_id, typeof station.station_id);
         if (index < MAX_POINTS) {       //snip-- && station.station_id.substring(0, 2) == 'US' 
             geojson_array.push(
                 {
